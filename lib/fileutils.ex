9c60c73e6dd2fa6b75f591c5b4dcc882702a8690
Michael Jochimsen <mlj@troglodytics.org>
2015-06-03 16:09:10 -0700

    Add install_file_tree/2 to the FileUtils module.
    install_file_tree/2 can be used to install a collection of file data in a
    directory tree structure, with the added ability of specifying the permissions
    on each file.

--- lib/fileutils.ex
+++ lib/fileutils.ex
@@ -1,2 +1,96 @@
 defmodule FileUtils do
+
+  @type posix :: :file.posix
+
+  @doc """
+  Create a tree of file / directory specifications under the given root. Each
+  file / directory has a name and and optional permission (default permissions
+  are `0o755` for directories and `0o644` for files). Files also contain a
+  binary which is written to the newly created file. Directories contain a list
+  of additional specifications, which are recursed to build a full directory
+  tree.
+
+  Example:
+
+      :ok = install(System.tmpdir, [
+        {"test-data", [
+          {"data", <<0, 1, 2, 3, 4>>},
+          {"read_only", 0o444, <<4, 3, 2, 1, 0>>},
+          {"no_access", 0o000, <<255, 255>>},
+          {"subdir", 0o555, [
+            {"more_data", "The quick brown fox..."}
+          ]}
+        ]}
+      ])
+
+
+  If the function succeeds, `:ok` is returned. If it fails then `{:error,
+  reason, pathname}` is returned with the reason being a `:posix` reason and the
+  pathname being the file / directory which caused the error (minus the root
+  directory portion).
+  """
+  @type entry :: {Path.t, non_neg_integer, binary} |
+                 {Path.t, binary} |
+                 {Path.t, non_neg_integer, [entry]} |
+                 {Path.t, [entry]}
+  @type install_tree_error :: {:error, posix, Path.t} | {:error, :badarg, term}
+  @spec install_file_tree(Path.t, [entry]) :: :ok | install_tree_error
+  def install_file_tree(rootdir, entries) when is_binary(rootdir) and is_list(entries) do
+    case File.mkdir_p(rootdir) do
+      :ok -> install_entry_list(rootdir, entries) |> trim_error_path(rootdir)
+      {:error, reason} -> {:error, reason, ""}
+    end
+  end
+
+  defp trim_error_path(:ok, _rootdir), do: :ok
+  defp trim_error_path({:error, :badarg, _term} = error, _rootdir), do: error
+  defp trim_error_path({:error, reason, pathname}, rootdir) do
+    {:error, reason, Path.relative_to(pathname, rootdir)}
+  end
+
+  defp install_entry_list(_workdir, []), do: :ok
+  defp install_entry_list(workdir, [entry | rest]) do
+    case install_entry(workdir, entry) do
+      :ok -> install_entry_list(workdir, rest)
+      {:error, _reason, _pathname} = error -> error
+    end
+  end
+
+  defp install_entry(workdir, {filename, perm, data}) when is_binary(filename) and perm in 0..0o777 and is_binary(data) do
+    pathname = Path.join(workdir, filename)
+    install_entry_using_actions(pathname, [
+      fn -> File.write(pathname, data, [:exclusive]) end,
+      fn -> File.chmod(pathname, perm) end
+    ])
+  end
+  defp install_entry(workdir, {dirname, perm, entries}) when is_binary(dirname) and perm in 0..0o777 and is_list(entries) do
+    pathname = Path.join(workdir, dirname)
+    install_entry_using_actions(pathname, [
+      fn -> File.mkdir(pathname) end,
+      fn -> install_entry_list(pathname, entries) end,
+      fn -> File.chmod(pathname, perm) end
+    ])
+  end
+  defp install_entry(workdir, {filename, data}) when is_binary(data) do
+    install_entry(workdir, {filename, 0o644, data})
+  end
+  defp install_entry(workdir, {dirname, entries}) when is_list(entries) do
+    install_entry(workdir, {dirname, 0o755, entries})
+  end
+  defp install_entry(_workdir, badarg) do
+    {:error, :badarg, badarg}
+  end
+
+  defp install_entry_using_actions(pathname, actions, result \\ :ok)
+  defp install_entry_using_actions(_pathname, [], :ok), do: :ok
+  defp install_entry_using_actions(pathname, [action | rest], :ok) do
+    install_entry_using_actions(pathname, rest, action.())
+  end
+  defp install_entry_using_actions(pathname, _actions, {:error, reason}) do
+    {:error, reason, pathname}
+  end
+  defp install_entry_using_actions(_pathname, _actions, {:error, _reason, _path} = error) do
+    error
+  end
+
 end
--- test/fileutils_test.exs
+++ test/fileutils_test.exs
@@ -1,3 +1,159 @@
 defmodule FileUtilsTest do
   use ExUnit.Case
+  use Bitwise
+
+  setup context do
+    unless context[:no_workdir] do
+      workdir = Path.join(System.tmp_dir, "#{context[:case]}:#{context[:line]}")
+      File.mkdir!(workdir)
+      on_exit(fn -> File.rm_rf(workdir) end)
+      {:ok, workdir: workdir}
+    else
+      :ok
+    end
+  end
+
+  test "creating a tree of files", context do
+    workdir = context[:workdir]
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"fox", 0o500, [
+        {"the", 0o400, "article"},
+        {"quick", "adjective"},
+        {"brown", "adjective"},
+      ]},
+      {"jumps", []},
+      {"over", [
+        {"dog", [
+          {"the", 0o400, "article"},
+          {"lazy", "adjective"},
+        ]},
+      ]},
+    ]) == :ok
+
+    fox_path = Path.join(workdir, "fox")
+    the1_path = Path.join(fox_path, "the")
+    quick_path = Path.join(fox_path, "quick")
+    brown_path = Path.join(fox_path, "brown")
+    jumps_path = Path.join(workdir, "jumps")
+    over_path = Path.join(workdir, "over")
+    dog_path = Path.join(over_path, "dog")
+    the2_path = Path.join(dog_path, "the")
+    lazy_path = Path.join(dog_path, "lazy")
+
+    assert File.dir?(fox_path)
+    assert File.ls!(fox_path) == ["brown", "quick", "the"]
+    assert {:ok, stat} = File.stat(fox_path)
+    assert (stat.mode &&& 0o777) == 0o500
+
+    assert File.regular?(the1_path)
+    assert File.read!(the1_path) == "article"
+    assert {:ok, stat} = File.stat(the1_path)
+    assert (stat.mode &&& 0o777) == 0o400
+
+    assert File.regular?(quick_path)
+    assert File.read!(quick_path) == "adjective"
+    assert {:ok, stat} = File.stat(quick_path)
+    assert (stat.mode &&& 0o777) == 0o644
+
+    assert File.regular?(brown_path)
+    assert File.read!(brown_path) == "adjective"
+    assert {:ok, stat} = File.stat(brown_path)
+    assert (stat.mode &&& 0o777) == 0o644
+
+    assert File.dir?(jumps_path)
+    assert File.ls!(jumps_path) == []
+    assert {:ok, stat} = File.stat(jumps_path)
+    assert (stat.mode &&& 0o777) == 0o755
+
+    assert File.dir?(over_path)
+    assert File.ls!(over_path) == ["dog"]
+    assert {:ok, stat} = File.stat(over_path)
+    assert (stat.mode &&& 0o777) == 0o755
+
+    assert File.dir?(dog_path)
+    assert File.ls!(dog_path) == ["lazy", "the"]
+    assert {:ok, stat} = File.stat(dog_path)
+    assert (stat.mode &&& 0o777) == 0o755
+
+    assert File.regular?(the2_path)
+    assert File.read!(the2_path) == "article"
+    assert {:ok, stat} = File.stat(the2_path)
+    assert (stat.mode &&& 0o777) == 0o400
+
+    assert File.regular?(lazy_path)
+    assert File.read!(lazy_path) == "adjective"
+    assert {:ok, stat} = File.stat(lazy_path)
+    assert (stat.mode &&& 0o777) == 0o644
+
+    # Make sure we can clean up the working directory behind us.
+    File.chmod(fox_path, 0o700)
+  end
+
+  test "creating a file (or directory) twice results in error", context do
+    workdir = context[:workdir]
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"hello", ""},
+      {"hello", ""},
+    ]) == {:error, :eexist, "hello"}
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"world", []},
+      {"world", []},
+    ]) == {:error, :eexist, "world"}
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"over", [
+        {"dog", [
+          {"brown", ""}
+        ]},
+        {"dog", [
+          {"black", ""}
+        ]},
+      ]},
+    ]) == {:error, :eexist, "over/dog"}
+  end
+
+  test "failure to create the root directory results in an error", context do
+    workdir = context[:workdir]
+    rod = Path.join(workdir, "read_only_dir")
+    install_dir = Path.join(rod, "install_dir")
+
+    File.mkdir(rod)
+    File.chmod(rod, 0o500)
+
+    assert FileUtils.install_file_tree(install_dir, [
+      {"hello", ""}
+    ]) == {:error, :eacces, ""}
+  end
+
+  test "that an invalid file tree description results in an error", context do
+    workdir = context[:workdir]
+
+    assert FileUtils.install_file_tree(workdir, [
+      {:goodbye, :cruel, :world}
+    ]) == {:error, :badarg, {:goodbye, :cruel, :world}}
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"foo"}
+    ]) == {:error, :badarg, {"foo"}}
+
+    assert FileUtils.install_file_tree(workdir, [
+      "foo"
+    ]) == {:error, :badarg, "foo"}
+  end
+
+  test "that out of range permissions result in an error", context do
+    workdir = context[:workdir]
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"foo", 0o1000, "bar"}
+    ]) == {:error, :badarg, {"foo", 0o1000, "bar"}}
+
+    assert FileUtils.install_file_tree(workdir, [
+      {"foo", -1, "bar"}
+    ]) == {:error, :badarg, {"foo", -1, "bar"}}
+  end
+
 end
